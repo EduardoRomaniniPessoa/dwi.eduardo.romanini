2dc82ab484ff7bf56c2cbe696e62f5eb835170ec
EduardoRomaniniPessoa <eduardo.pessoa@etec.sp.gov.br>
2024-08-13 21:35:41 -0300

    acesso ao github

--- acesso git bash.bat
+++ acesso git bash.bat
@@ -7,7 +7,7 @@ git config --global --unset-all user.name
 
 echo --------------------------------------------------------
 echo Configurar Nome de Usuario
-echo config --global user.name Eduardo Romanini Pessoa
+echo config --global user.name EduardoRomaniniPessoa
 echo
 echo --------------------------------------------------------
 echo Configurar conta do usuario
---
+++ clone.bat
@@ -0,0 +1,8 @@
+echo OFF
+
+echo --- Clonando Pasta do Git HUB ----
+
+cd C:\Users\pc\Desktop
+git clone https://github.com/EduardoRomaniniPessoa/dwi.eduardo.romanini.git
+
+pause
