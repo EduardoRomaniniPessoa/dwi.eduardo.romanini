0063adc2979910859a5a7db70165ea4c123dad5f
unknown <eduardopessoa@etec.sp.gov.br>
2024-08-16 22:28:59 -0300

    projetinhos do dia 16

--- acesso git bash.bat
+++ acesso git bash.bat
@@ -7,17 +7,9 @@ git config --global --unset-all user.name
 
 echo --------------------------------------------------------
 echo Configurar Nome de Usuario
-echo config --global user.name EduardoRomaniniPessoa
-echo
+echo config --global user.name "EduardoRomaniniPessoa"
 echo --------------------------------------------------------
 echo Configurar conta do usuario
-echo config --global user.email eduardo.pessoa@etec.sp.gov.br
-echo --------------------------------------------------------
-
-echo----------------------- Configuração atual do GitHub no computador--------------------
-git config --list
-echo --- luz fa de artic monkey ---
-echo --- xoxo ---
-
+echo config --global user.email "eduardo.pessoa@etec.sp.gov.br"
 
 pause
--- clone.bat
+++
@@ -1,8 +0,0 @@
-echo OFF
-
-echo --- Clonando Pasta do Git HUB ----
-
-cd C:\Users\pc\Desktop
-git clone https://github.com/EduardoRomaniniPessoa/dwi.eduardo.romanini.git
-
-pause
