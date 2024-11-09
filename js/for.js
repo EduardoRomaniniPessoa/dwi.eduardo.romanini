9b868c21da16f87962d7e2b1a6ca6df5bdc059a5
eduu <eduardo.pessoa@etec.sp.gov.br>
2024-11-08 22:21:00 -0300

    Add files via upload

---
+++ js/for.js
@@ -0,0 +1,18 @@
+const cars = ["BMW", "Volvo","Saab", "Ford", "Fiat",
+"Audi"];
+'   '
+let text = "";
+for (let i = 0; i <cars.length; i++) {
+    text += cars[i];
+    console.log(`Agora foi colocado o ${cars[i]}
+         na variável text`)
+}
+console.log(text);
+console.log('---------------------------------------');
+console.log('---------------------------------------');
+
+cars.forEach((car,i) => {
+    console.log(
+        `O carro ${car} está na posição ${i} do array`
+    )
+})
